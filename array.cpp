50ee8f0634d966a86aa939f8a39adc9983834106
azhar64888 <azharjamal648@gmail.com>
2025-10-24 13:46:23 +0500

    Lab 5 Cpp File

---
+++ array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {5, 3, 8, 1, 2};
+    int n = 5;
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    cout << "Before Sorting: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "After Sorting: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    cout << "Maximum: " << max << endl;
+    cout << "Minimum: " << min << endl;
+
+    return 0;
+}
